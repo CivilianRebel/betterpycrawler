3c5db8466e603dbcfdd2f1ee6ca9c38cba8b3d52
civilianrebel <steventayloroo@gmail.com>
2020-01-01 20:38:57 -0500

    this might work, can't test rn

--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
 import hashlib
+from urllib.parse import urlparse, urljoin
+from url import Url as UrlClass
+
+def process_links(links):
+    final_links = []
+    for link in links:
+        url_ob = UrlClass(url=link)
+        parsed = urlparse(str(url_ob))
+        if parsed.scheme is None:
+            # must be a local link
+            host = url_ob.host
+            if not re.match(regex, str(url_ob)) is None:
+                final_links.append(urljoin(host, str(url_ob)))
 
 
 def hash_func(s):
